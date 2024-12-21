b64fe8ae361301ecb354ebdd9342cac1835f1995
satyamgupta09 <138917027+satyamgupta09@users.noreply.github.com>
2024-12-21 20:49:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const byteSize = (str) => {
   // write your code here
-	return str.length;
+	let blobObject = new Blob([str]);
+	return blobObject.size;
 };
 
-// Do not change the code below 
-const str = prompt("Enter some string.");
+// Do not change the code below  
+// const str = prompt("Enter some string.");
 alert(byteSize(str));
+ 
